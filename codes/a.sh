d1ccfb82f73e99ca3d512a9510b07ca9e4099ed8
nineheart6 <121921855+nineheart6@users.noreply.github.com>
2025-07-14 17:59:14 +0900

    첫 과제

---
+++ codes/a.sh
@@ -0,0 +1,8 @@
+[seungjae@localhost Music]$ cd /home/seungjae/Pictures
+[seungjae@localhost Pictures]$ pwd
+/home/seungjae/Pictures
+[seungjae@localhost Pictures]$ ls
+[seungjae@localhost Pictures]$ cd /home/seungjae/Pictures
+[seungjae@localhost Pictures]$ pwd
+/home/seungjae/Pictures
+[seungjae@localhost Pictures]$ ls
