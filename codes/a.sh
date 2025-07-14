c5ef6ece18033d4d2d349d46824f1e494dace981
nineheart6 <121921855+nineheart6@users.noreply.github.com>
2025-07-14 18:01:05 +0900

    Delete codes/a.sh

--- codes/a.sh
+++
@@ -1,8 +0,0 @@
-[seungjae@localhost Music]$ cd /home/seungjae/Pictures
-[seungjae@localhost Pictures]$ pwd
-/home/seungjae/Pictures
-[seungjae@localhost Pictures]$ ls
-[seungjae@localhost Pictures]$ cd /home/seungjae/Pictures
-[seungjae@localhost Pictures]$ pwd
-/home/seungjae/Pictures
-[seungjae@localhost Pictures]$ ls
